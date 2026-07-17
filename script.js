a38005ff0b4ec9e3b342600e1faf244423f11f6f
7796kardilepriynka <106344900+7796kardilepriynka@users.noreply.github.com>
2026-07-17 12:33:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.getElementById("navLinks");
 
+// Toggle menu
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+// Close menu after clicking a link (Mobile only)
+document.querySelectorAll("#navLinks a").forEach(link => {
+  link.addEventListener("click", () => {
+    if (window.innerWidth <= 768) {
+      navLinks.classList.remove("active");
+    }
+  });
+});;
